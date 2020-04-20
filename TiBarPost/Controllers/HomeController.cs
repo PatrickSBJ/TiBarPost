bedc7de8502c1abcdb00c775370e1d2a8de6e490
PatrickSBJ <behrensjungferp@my.lanecc.edu>
2020-04-20 12:49:42 -0700

    first commit

---
+++ TiBarPost/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TiBarPost.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to TiBär Post!";
+
+            return View();
+        }
+
+    }
+}
---
+++ TiBarPost/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TiBarPost.Startup))]
+namespace TiBarPost
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
